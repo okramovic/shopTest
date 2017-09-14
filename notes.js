c95710506d178faa2dabdbd61bbc918ebb24dcdb
okram ovic <okram@protonmail.ch>
2017-09-14 11:57:19 +0300

    update notes.js to contain basic git instructions

--- notes.js
+++ notes.js
@@ -2,6 +2,24 @@
 // GIT
 //https://github.com/Kunena/Kunena-Forum/wiki/Create-a-new-branch-with-git-and-manage-branches
 
+// 		saving changes
+
+//			https://www.atlassian.com/git/tutorials/saving-changes
+/*
+			git add .		(or filenames)
+			git commit -m “some message”
+
+
+			to merge into master:
+			1.step:	checkout to master
+			2.step:	git merge —no-ff “name of branch”
+
+
+
+*/
+
+
+
 //	merging branches
 //	https://www.atlassian.com/git/tutorials/git-merge
 
